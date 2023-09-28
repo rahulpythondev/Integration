1610a2123e7b9bb20eaeaa95a89916508ef0c9bf
rahul pythondev <learningpythonbyrahul@gmail.com>
2023-09-28 18:30:59 +0000

    Initial Draft

---
+++ code/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from barfi import st_barfi, Block
+
+# Title and page config
+st.set_page_config(
+    layout='wide',
+    page_title="Demo React Flow",
+    initial_sidebar_state="collapsed"
+)
+
+# CSS to hide Streamlit elements
+hide_streamlit_style = """
+<style>
+    div.block-container {padding-top:2rem;}
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+</style>
+"""
+
+feed = Block(name='Input')
+feed.add_input(name="Hello")
+
+result = Block(name='Result')
+result.add_output()
+
+barfi_result = st_barfi(base_blocks=[feed, result])
+
+if barfi_result:
+    st.write(barfi_result)
